2c8397196c1ba61b5eb161b0e4a6d601bdc5265e
nino-coder <nino.giorgadze.2000@gmail.com>
2020-05-30 15:09:23 +0400

    new fixes

--- src/components/currentWeather/kutaisi.js
+++ src/components/currentWeather/kutaisi.js
@@ -6,7 +6,7 @@ export default class Kutaisi  extends Component{
     }
     render(){
         return(
-            <div class="city">
+            <div className="city">
                         <center>  <h4>{this.props.city}</h4></center>
                         <time>{this.props.date}</time>
                         <h1>{Math.round(this.props.celsius*100)/100}<sup>0</sup>{this.props.cels===true?"C":"F"}</h1>
@@ -14,7 +14,7 @@ export default class Kutaisi  extends Component{
                         <li>max temp:{Math.round(this.props.max*100)/100}<sup>0</sup>{this.props.cels===true?"C":"F"}</li>
                         <li>min temp:{Math.round(this.props.min*100)/100}<sup>0</sup>{this.props.cels===true?"C":"F"}</li>
                         <li>humidity:{this.props.main.humidity}</li>
-                        <li><b class="description">{this.props.weather.description}</b></li> 
+                        <li><b className="description">{this.props.weather.description}</b></li> 
                     </ul>
                 </div>
         )
--- src/components/dailyWeather/batumiDaily.js
+++ src/components/dailyWeather/batumiDaily.js
@@ -10,13 +10,13 @@ export default class DailyWeather extends Component{
     render(){
         const celsius = this.props.celsius?this.props.list.main.temp-273.15:this.props.list.main.temp*9/5-459.67
         return(
-        <div class="daily">
+        <div className="daily">
             <time>{this.props.list.dt_txt}</time>
             <h1>{Math.round(celsius*100)/100}<sup>0</sup>{this.props.celsius===true?"C":"F"}</h1>
             <ul>
                 <li>humidity:{this.props.list.main.humidity}</li>
                 <li>wind speed:{this.props.list.wind.speed}</li>
-                <li><b class="description">{this.props.list.weather[0].description}</b></li>
+                <li><b className="description">{this.props.list.weather[0].description}</b></li>
             </ul>
         </div>
     )
